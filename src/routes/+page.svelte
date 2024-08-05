148709e86f4ba94e44b12b8a2c05db9f21311aaa
JasurAvazov <jasuravazov4@gmail.com>
2024-08-05 17:01:21 +0500

    fix

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<a href="/" class="btn-icon bg-gradient-to-br variant-gradient-secondary-primary">skltn</a>
-<a href="/" class="btn bg-gradient-to-br variant-gradient-secondary-primary">wow</a>
-<a href="/" class="btn bg-gradient-to-br variant-gradient-secondary-primary">
-	<span>hello</span>
-	<span>world</span>
-</a>
---
+++ src/routes/feed/+page.svelte
@@ -0,0 +1,44 @@
+<div class="feed">
+  <header class="header">
+    <div class="header__container">
+      <h1>dsadasds</h1>
+    </div>
+  </header>
+  <div class="posts">
+    <h1 class="post">dasdasd</h1>
+  </div>
+  <h1>dsadasdas</h1>
+</div>
+
+<style lang="scss">
+  .feed {
+    
+  }
+
+  .header {
+    width: 100%;
+    background-color: red;
+    margin-bottom: 20px;
+
+    &__container {
+      max-width: 800px;
+      width: 100%;
+      margin: auto;
+      padding: 20px;
+    }
+  }
+  
+  .posts {
+    max-width: 800px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: auto;
+  }
+
+  .post {
+    width: 100%;
+    padding: 20px;
+    background-color: green;
+  }
+</style>
