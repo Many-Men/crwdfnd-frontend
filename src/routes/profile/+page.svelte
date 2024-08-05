f1a3d879e8c478f878a763f1a9abf117c481ecb4
monov <shohruhulous@gmail.com>
2024-08-05 19:06:17 +0500

    fix

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,15 +3,15 @@
     <div class="profile-pfp"></div>
     <h1>Username</h1>
     <div class="profile-menu">
-      <div class="profile-menu-item">Liked</div>
-      <div class="profile-menu-item">My Campaigns</div>
+      <a class="profile-menu-item" href="/profile/liked">Liked</a>
+      <a class="profile-menu-item" href="/profile/mycamps">My Campaigns</a>
     </div>
   </div>
 </div>
 
 <style lang="scss">
   .profile-body{
-    background: #eee;
+    background: #fff;
     height: 100vh;
     display: flex;
     align-items: center;
@@ -21,12 +21,11 @@
   .profile-pfp{
     height: 200px;
     width: 200px;
-    background: rgb(0, 123, 255);
+    background: #4169e0;
     border-radius: 50%;
   }
 
   .profile-main-form{
-    
     width: 400px;
     border-radius: 10px;
     display: flex;
@@ -41,6 +40,27 @@
     width: 100%;
     border-radius: 10px;
     background: #fff;
-    box-shadow: 0 0 10px #808080;
+    border: solid 1px #4169e0;
+  }
+
+  .profile-menu-item{
+    height: 40px;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+    margin-bottom: 5px;
+    transition: .1s;
+    cursor: pointer;
+    text-decoration: none;
+    color: #000;
+  }
+
+  .profile-menu-item:last-of-type{
+    margin: 0px;
+  }
+
+  .profile-menu-item:hover{
+    background: #eee;
   }
 </style>
---
+++ src/routes/profile/liked/+page.svelte
@@ -0,0 +1,115 @@
+<script>
+  let data = [
+    {
+      campaign_id: "123abc",
+      title: "Save the Rainforest",
+      description: "A campaign to raise funds for rainforest conservation.",
+      goal: 50000.0,
+      current_amount: 23000.5,
+      creator: "nature_lover89",
+      like_count: 1200,
+      created_at: "2024-08-05T14:30:00Z",
+    },
+    {
+      campaign_id: "123abc",
+      title: "Save the Rainforest",
+      description: "A campaign to raise funds for rainforest conservation.",
+      goal: 50000.0,
+      current_amount: 23000.5,
+      creator: "nature_lover89",
+      like_count: 1200,
+      created_at: "2024-08-05T14:30:00Z",
+    },
+    {
+      campaign_id: "123abc",
+      title: "Save the Rainforest",
+      description: "A campaign to raise funds for rainforest conservation.",
+      goal: 50000.0,
+      current_amount: 23000.5,
+      creator: "nature_lover89",
+      like_count: 1200,
+      created_at: "2024-08-05T14:30:00Z",
+    },
+    {
+      campaign_id: "123abc",
+      title: "Save the Rainforest",
+      description: "A campaign to raise funds for rainforest conservation.",
+      goal: 50000.0,
+      current_amount: 23000.5,
+      creator: "nature_lover89",
+      like_count: 1200,
+      created_at: "2024-08-05T14:30:00Z",
+    },
+    {
+      campaign_id: "123abc",
+      title: "Save the Rainforest",
+      description: "A campaign to raise funds for rainforest conservation.",
+      goal: 50000.0,
+      current_amount: 23000.5,
+      creator: "nature_lover89",
+      like_count: 1200,
+      created_at: "2024-08-05T14:30:00Z",
+    },
+    {
+      campaign_id: "123abc",
+      title: "Save the Rainforest",
+      description: "A campaign to raise funds for rainforest conservation.",
+      goal: 50000.0,
+      current_amount: 23000.5,
+      creator: "nature_lover89",
+      like_count: 1200,
+      created_at: "2024-08-05T14:30:00Z",
+    },
+  ];
+</script>
+
+<div class="liked-body">
+  <div class="liked-items-wrapper">
+    {#each data as item}
+      <div class="liked-item">
+        <div class="liked-pic"></div>
+        <div class="liked-title">{item.title}</div>
+        <div class="liked-desc">{item.description}</div>
+        <div>Goal: ${item.current_amount}/${item.goal}</div>
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .liked-body{
+    display: flex;
+    justify-content: center;
+  }
+  .liked-items-wrapper{
+    display: grid;
+    grid-template-columns: 3fr 3fr 3fr;
+    justify-items: center;
+    width: 60%;
+  }
+
+  .liked-item{
+    border-radius: 10px;
+    border: solid 1px #4169e0;
+    width: 300px;
+    padding: 5px;
+    margin-top: 10px;
+    background: #fff;
+  }
+
+  .liked-pic{
+    height: 160px;
+    width: 100%;
+    background: #4169e0;
+    border-radius: 5px;
+  }
+
+  .liked-title{
+    font-size: 18px;
+    font-weight: 600;
+  }
+
+  .liked-desc{
+    font-size: 14px;
+  }
+</style>
